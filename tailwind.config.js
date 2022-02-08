a60ef2bbe810ca467e7b97ce22e5f2b147232dd9
LAHILOTE <mudher.d@gmail.com>
2022-02-08 11:18:44 +0800

    Adding Custom Collor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      colors: {
+        primaryColor: '#F15FA3',
+      secondaryColor: '#B6C7E8',
+      tertiaryColor: '#AB94CE',
+      textColor: '#393C43'
+      }
     },
   },
   plugins: [],
